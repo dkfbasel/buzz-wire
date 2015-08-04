e41cb67d2450c03bf3d22697e788414fa3899416
tikiatua <tikiatua@gmail.com>
2015-08-04 10:48:14 +0200

    move initialisation to a separate file

---
+++ init.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// initialize the configuration
+func initConfiguration() {
+
+	// try to load some configuration information from the config file
+	viper.SetConfigName("config")
+	viper.SetDefault("mode", MODE_PI)
+	viper.SetDefault("timeout", 5*time.Second)
+	viper.SetDefault("debounceContact", 1000*time.Millisecond)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	// read configuration from file or use default
+	Timeout = viper.GetDuration("timeout")
+	DebounceContact = viper.GetDuration("debounceContact")
+	Mode = viper.GetString("mode")
+
+	// create a new lock for concurrent access
+	Mutex = &sync.RWMutex{}
+
+	// initialize the game state
+	setState(IS_STOPPED)
+
+	// initialize our communication channels
+	// note: the contactChannel is debounced to handle continuous triggering of the contact
+	contactChannel = make(chan bool)
+	contactChannelDebounced = debounceContactChannel(DebounceContact, contactChannel)
+
+	// we need a signal channel to communicate events to the webserver
+	signalChannel = make(chan string)
+
+	// handle start events
+	startChannel = make(chan bool)
+
+	// handle finish events
+	finishChannel = make(chan StopReason)
+
+}
--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
-
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/platforms/gpio"
 	"github.com/hybridgroup/gobot/platforms/raspi"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -21,6 +16,7 @@ var GameEvents chan string
 
 func main() {
 
+	// initialize our base configuration for the system
 	initConfiguration()
 
 	// initialize a new gobot
@@ -51,72 +47,8 @@ func main() {
 	// simulate events with keyboard interaction
 	go simulate(GameEvents)
 
-	// define the work to be done by the robot
-	work := func() {
-
-		// user pushed the start/stop button
-		gobot.On(buttonWoman.Event("push"), func(data interface{}) {
-			handleButtonPress(FEMALE)
-		})
-
-		// gobot.On(buttonMan.Event("push"), func(data interface{}) {
-		// 	handleButtonPress(MALE)
-		// })
-
-		// user made contact with wire
-		gobot.On(contactWire.Event("push"), handleWireContact)
-
-		// user is starting the game (must touch the starting area)
-		// TODO: add handler for starting event
-		// gobot.On(contactStart.Event("push"), handleStartContact)
-
-		// user finished the game (touched finish area)
-		gobot.On(contactFinish.Event("push"), handleFinishContact)
-
-		go func() {
-
-			for event := range GameEvents {
-
-				switch event {
-				// sound the buzzer
-				case "soundBuzzer":
-					go func() {
-						buzzer.On()
-						<-time.After(300 * time.Millisecond)
-						buzzer.Off()
-					}()
-
-				// enable/disable the led for the woman button
-				case "enableLedWoman":
-					ledWoman.On()
-				case "disableLedWoman":
-					ledWoman.Off()
-
-				// enable/disable the lef for the man button
-				// case "enableLedMan":
-				// 	ledMan.On()
-				// case "disableLedMan":
-				// 	ledMan.Off()
-
-				// disable all leds
-				case "ledOff":
-					ledWoman.Off()
-					// ledMan.Off()
-
-				// simulated events
-				case "button":
-					handleButtonPress(FEMALE)
-				case "contact":
-					handleWireContact(nil)
-				case "start":
-					handleStartContact(nil)
-				case "finish":
-					handleFinishContact(nil)
-				}
-			}
-		}()
-
-	}
+	// define the work to be done by the robot (i.e. react to events)
+	work := eventHandling()
 
 	// we need to define a robot to be used with gobot
 	var robot *gobot.Robot
@@ -144,42 +76,4 @@ func main() {
 
 	// start the robot (blocking)
 	gbot.Start()
-}
-
-// initialize the configuration
-func initConfiguration() {
-
-	// try to load some configuration information from the config file
-	viper.SetConfigName("config")
-	viper.SetDefault("mode", MODE_PI)
-	viper.SetDefault("timeout", 5*time.Second)
-	viper.SetDefault("debounceContact", 1000*time.Millisecond)
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-
-	// read configuration from file or use default
-	Timeout = viper.GetDuration("timeout")
-	DebounceContact = viper.GetDuration("debounceContact")
-	Mode = viper.GetString("mode")
-
-	// create a new lock for concurrent access
-	Mutex = &sync.Mutex{}
-
-	// initialize the game state
-	setState(IS_STOPPED)
-
-	// initialize our communication channels
-	// note: the contactChannel is debounced to handle continuous triggering of the contact
-	contactChannel = make(chan bool)
-	contactChannelDebounced = debounceContactChannel(DebounceContact, contactChannel)
-
-	// we also need a signal channel to communicate events to the webserver
-	signalChannel = make(chan string)
-
-	// handle finish events
-	finishChannel = make(chan StopReason)
-
 }
